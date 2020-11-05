1be54bb9921cc8ac61b8a60a4243c98b73192dc3
Kostia Netot <pugjazz@gmail.com>
2020-11-05 10:44:03 +0200

    adding new news

---
+++ js/News.js
@@ -0,0 +1,11 @@
+
+class News {
+  constructor(id, title, reporter, date, category, text) {
+    this.title = title;
+    this.reporter = reporter;
+    this.date = date;
+    this.category = category;
+    this.text = text;
+    this.id = id;
+  }
+}
---
+++ js/Store.js
@@ -0,0 +1,28 @@
+
+class Store {
+  static getAllNews() {
+    let allNews;
+    if (localStorage.getItem('allNews') === null) {
+      localStorage.setItem('allNews', JSON.stringify(initialNewsData));
+      allNews = JSON.parse(localStorage.getItem('allNews'));
+    } else {
+      allNews = JSON.parse(localStorage.getItem('allNews'));
+    }
+    return allNews;
+  }
+
+  static addNews(news) {
+    const allNews = Store.getAllNews();
+    allNews.push(news);
+    localStorage.setItem('allNews', JSON.stringify(allNews));
+  }
+
+  static removeNews(id) {
+    const allNews = Store.getAllNews();
+    allNews.forEach((news, index) => {
+      allNews.splice(index, 1);
+    });
+    localStorage.setItem('allNews', JSON.stringify(allNews));
+  }
+
+}
